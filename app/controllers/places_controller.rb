26c80c229329050e926bec000072a962f77ec51e
Igor Fernandes <fernandesigor.bcn@icloud.com>
2024-09-03 14:47:33 -0300

    Controller index places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,13 @@
 class PlacesController < ApplicationController
+  before_action :set_city, only: %i[index]
+
+  def index
+    @places = Place.where(city: @city)
+  end
+
+  private
+
+  def set_city
+    @city = City.find(params[:city_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,16 +29,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_180403) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "itineraries", force: :cascade do |t|
-    t.text "suggestion"
-    t.bigint "place_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["place_id"], name: "index_itineraries_on_place_id"
-    t.index ["user_id"], name: "index_itineraries_on_user_id"
-  end
-
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -82,8 +72,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_02_180403) do
   end
 
   add_foreign_key "cities", "countries"
-  add_foreign_key "itineraries", "places"
-  add_foreign_key "itineraries", "users"
   add_foreign_key "places", "cities"
   add_foreign_key "plans", "places"
   add_foreign_key "plans", "trips"
