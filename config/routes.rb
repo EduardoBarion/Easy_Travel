1019688e0f115cb17e9e127d812f9fb5c0b11260
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-09-03 18:04:13 -0300

    rotas mudadas

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :countries, only: %i[index]
-
-  resources :cities, only: :index do
-    resources :places, only: %i[index, show]
+  resources :countries, only: :index do
+    resources :cities, only: :index do
+      resources :places, only: %i[index show]
+    end
   end
   resources :trips, only: %i[index]
 
