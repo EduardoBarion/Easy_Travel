c18acd044214df31f388c957a4ed8ea0a20a45e6
Lucas Abib <lucas.abib@gmail.com>
2024-09-03 20:38:29 +0200

    criação router-controller-index-countries

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :countries, only: %i[index]
+
   resources :places, only: %i[show]
 
   # Defines the root path route ("/")
