3cd1fb7f6b9c48a5fd13a654c6e8819338c6aee1
Igor Fernandes <fernandesigor.bcn@icloud.com>
2024-09-03 15:26:48 -0300

    controller view places

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,20 @@
 class PlansController < ApplicationController
+
+  def create
+    @place = Place.find(params[:places_id])
+    @plan = Plan.new(plan_params)
+    @plan.place = @place
+    @place = current_place
+    if @plan.save
+      render turbo_stream: turbo_stream.replace("plan_form", partial: "plans/confirmed")
+    else
+      render turbo_stream: turbo_stream.replace("plan_form", partial: "plans/form", locals: {places: @places, plan: @plan})
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:suggestion)
+  end
 end
