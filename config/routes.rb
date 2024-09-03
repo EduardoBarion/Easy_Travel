7e6a65518a4818a9b54c59da3421855011d7a63e
David Andrade <david.andradejr@icloud.com>
2024-09-03 15:58:28 -0300

    index de cities finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'cities', to: 'cities#index'
+  #get 'cities', to: 'cities#index'
+
+  resources :cities, only: %i[index]
 
   resources :places, only: %i[show]
 
