2c94edbc6647d590ee8d5ee375b5d7289070e599
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-09-04 14:17:50 -0300

    Começando a fazer o modal para create trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,21 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def create
+    @plan = Plan.find(params[:plan_id])
+    @trip = Trip.new(trip_params)
+    @trip.plan = @trip
+    if @trip.save
+      render turbo_stream: turbo_stream.replace("trip_form", partial: "trips/confirmed")
+    else
+      render turbo_stream: turbo_stream.replace("trip_form", partial: "trips/form", locals: { plan: @plan, trip: @trip })
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:user_id)
+  end
 end
