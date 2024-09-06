f48c7e04cc4748c72be6b2c1a3f78d2f8eafe542
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-09-06 12:43:15 -0300

    user photos added

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,4 +2,10 @@ class CountriesController < ApplicationController
   def index
     @countries = Country.all
   end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:photo)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def user_params
+    params.require(:user).permit(:photo)
+  end
 end
