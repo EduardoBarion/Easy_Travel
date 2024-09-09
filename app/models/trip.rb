54248854e1507ca320ca571793bc30d2ab059548
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-09-09 17:41:55 -0300

    link para adicionar amigos

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,14 +1,21 @@
+require 'securerandom'
+
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :plans, dependent: :destroy
   has_many :places, through: :plans
   has_many :groups, dependent: :destroy
 
+  before_create :generate_invite_token
   after_create :create_initial_group
 
   validates :name, presence: true
 
   private
+
+  def generate_invite_token
+    self.invite_token = SecureRandom.hex(10)
+  end
 
   def create_initial_group
     groups.create!
