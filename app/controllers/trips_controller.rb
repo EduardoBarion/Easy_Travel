f4d074016005ec9d797682ed319e15e0795004f4
Eduardo Barion <eduardobarion2004@hotmail.com>
2024-09-10 11:11:43 -0300

    username e não email, além de link não duplicado

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -25,7 +25,7 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
     @group = Group.find_by(trip: @trip)
     @membership = Membership.new
-    @users = User.pluck(:id, :email)
+    @users = User.pluck(:id, :username)
     @plans = Plan.where(trip: @trip)
     @markers = @plans.map do |plan|
       { lat: plan.place.latitude, lng: plan.place.longitude }
